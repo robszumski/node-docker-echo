cb538dcf1546ccdbf6e141f89e6bd08699428a5e
Rob Szumski <rob@robszumski.com>
2019-10-23 22:22:35 -0400

    Record CPU and RAM

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
 const hostname = '0.0.0.0';
 const port = 3000;
@@ -8,9 +9,50 @@ const server = http.createServer((req, res) => {
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
 
-    var message = (query && query.message) ? query.message : 'Hello World';
+    var cpu;
+    var ram;
+
+    // Write parameters if set, else read
+    if(query.cpu) {
+        console.log(`Writing CPU as ${query.cpu}`);
+        cpu = query.cpu;
+
+        //Clear file
+        fs.unlink('./cpu', (err) => {
+          if (err) {
+            console.error(err)
+            return
+          }
+        })
+
+        fs.createWriteStream('./cpu').write(query.cpu);
+    } else if(fs.existsSync('./cpu')) {
+        cpu = fs.readFileSync('./cpu', 'utf8');
+        console.log(`Read CPU as ${cpu}`);
+    }
+
+    if(query.ram) {
+        console.log(`Writing RAM as ${query.ram}`);
+        ram = query.ram;
+
+        //Clear file
+        fs.unlink('./ram', (err) => {
+          if (err) {
+            console.error(err)
+            return
+          }
+        })
+
+        fs.createWriteStream('./ram').write(query.ram);
+    } else if (fs.existsSync('./ram')) {
+        ram = fs.readFileSync('./ram', 'utf8');
+        console.log(`Read RAM as ${ram}`);
+    }
+
+    // Return parameters
     var obj = {
-        'message': message,
+        'cpu': cpu,
+        'ram': ram,
         'env': process.env
     };
 
