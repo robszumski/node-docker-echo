8ce1c350ef10b4c4c63e7fad144b4d49103e3b53
Rob Szumski <rob@robszumski.com>
2020-07-10 22:34:54 -0400

    Track history

--- server.js
+++ server.js
@@ -9,8 +9,20 @@ const server = http.createServer((req, res) => {
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
 
+    var debug = true;
+
     var cpu;
     var ram;
+    var today_ram;
+    var yesterday_ram;
+    var change;
+
+    var today = new Date().getDay();
+    var yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    yesterday = yesterday.getDay();
+    if (yesterday == 0 || yesterday == 7) yesterday = 6; //if weekend, set to friday
+    if(debug) console.log("Today is " + today + ", yesterday is " + yesterday);
 
     // Write parameters if set, else read
     if(query.cpu && query.cpu >= -25) {
@@ -18,17 +30,17 @@ const server = http.createServer((req, res) => {
         cpu = query.cpu;
 
         //Clear file
-        fs.unlink('./cpu', (err) => {
+        fs.unlink('./' + today + '-cpu', (err) => {
           if (err) {
             console.error(err)
             return
           }
         })
 
-        fs.createWriteStream('./cpu').write(query.cpu);
-    } else if(fs.existsSync('./cpu')) {
-        cpu = fs.readFileSync('./cpu', 'utf8');
-        //console.log(`Read CPU as ${cpu}`);
+        fs.createWriteStream('./' + today + '-cpu').write(query.cpu);
+    } else if(fs.existsSync('./' + today + '-cpu')) {
+        cpu = fs.readFileSync('./' + today + '-cpu', 'utf8');
+        if(debug) console.log(`Read CPU as ${cpu}`);
     }
 
     if(query.ram && query.ram >= -1000) {
@@ -36,23 +48,34 @@ const server = http.createServer((req, res) => {
         ram = query.ram;
 
         //Clear file
-        fs.unlink('./ram', (err) => {
+        fs.unlink('./' + today + '-ram', (err) => {
           if (err) {
             console.error(err)
             return
           }
         })
 
-        fs.createWriteStream('./ram').write(query.ram);
-    } else if (fs.existsSync('./ram')) {
-        ram = fs.readFileSync('./ram', 'utf8');
-        //console.log(`Read RAM as ${ram}`);
+        fs.createWriteStream('./' + today + '-ram').write(query.ram);
+    } else if (fs.existsSync('./' + today + '-ram')) {
+        ram = fs.readFileSync('./' + today + '-ram', 'utf8');
+        if(debug) console.log(`Read RAM as ${ram}`);
+    }
+
+    // Calculate change
+    if (fs.existsSync('./' + today + '-ram') && fs.existsSync('./' + yesterday + '-ram')) {
+        today_ram = fs.readFileSync('./' + today + '-ram', 'utf8');
+        yesterday_ram = fs.readFileSync('./' + yesterday + '-ram', 'utf8');
+        change = today_ram - yesterday_ram;
+        if(debug) console.log(`Read RAM change as ${today_ram} (day ${today}) minus ${yesterday_ram} (day ${yesterday})= ${change}`);
+    } else {
+        if(debug) console.log(`Can't read RAM change, no history`);
     }
 
     // Return parameters
     var obj = {
         'cpu': cpu,
-        'ram': ram
+        'ram': ram,
+        'change': change
         //'env': process.env
     };
 
